125997e6560b9b0617cfb08b2c2cd671cdece07b
Miguel Vivas <savivdrid7@gmail.com>
2024-09-11 19:13:04 -0300

    2da pre entrega curso javascript

--- js/main.js
+++ js/main.js
@@ -1,55 +1,178 @@
 console.log("Bienvenid@ a Elixir")
 
-let nombre;
+let users = []
 
-do {
-    nombre= prompt("Ingresá tu nombre").trim()
-    if (!nombre) {
-        alert("Nombre invalido. Por favor, ingresá un nombre valido");
-    }
-} while (!nombre);
-
-let apellido;
-
-do {
-    apellido = prompt("Ingresá tu apellido").trim();
-    if (!apellido) {
-        alert("Apellido invalido. Por favor, ingresá un apellido valido");
-    } 
-} while (!apellido);
-
-let nombreCompleto = nombre.toUpperCase() + " " + apellido.toUpperCase()
-
-console.log("Hola " + nombreCompleto + " aca vendemos bebidas")
-
-function calcularEdad(fechaNacimiento) {
-    const hoy = new Date();
-    const [dia, mes, anio] = fechaNacimiento.split('/').map(Number);
-    if (isNaN(dia) || isNaN(mes) || isNaN(anio) || dia < 1 || dia > 31 || mes < 1 || mes > 12 || anio > hoy.getFullYear()) {
+function ageCalculate(birth) {
+    const today = new Date();
+    const [day, month, year] = birth.split('/').map(Number);
+    if (isNaN(day) || isNaN(month) || isNaN(year) || day < 1 || day > 31 || month < 1 || month > 12 || year > today.getFullYear()) {
         return null;
     }
-    const fechaNac = new Date(anio, mes -1, dia);
-    let edad = hoy.getFullYear() - fechaNac.getFullYear();
-    const mesActual = hoy.getMonth() - fechaNac.getMonth();
-    if (mesActual < 0 || (mesActual === 0 && hoy.getDate() < fechaNac.getDate())){
-        edad--;
+    const birthdate = new Date(year, month -1, day);
+    let age = today.getFullYear() - birthdate.getFullYear();
+    const actualMonth = today.getMonth() - birthdate.getMonth();
+    if (actualMonth < 0 || (actualMonth === 0 && today.getDate() < birthdate.getDate())){
+        age--;
     }
-    return edad;
+    return age;
 }
 
-let fechaNacimiento;
-let edad;
+function addUser() {
+    let email = prompt("Ingrese su correo electrónico");
+    let name;
+    do {
+        name = prompt("Ingresá tu nombre").trim()
+        if (!name) {
+            alert("Nombre invalido. Por favor, ingresá un nombre valido");
+        }
+    } while (!name);
 
-do {
-    fechaNacimiento = prompt ("Ingresá tu fecha de nacimiento (dd/mm/aaaa)");
-    edad = calcularEdad(fechaNacimiento);
-    if (edad === null) {
-        alert("Por favor, ingresá una fecha válida en el formato dd/mm/aaaa");
-    }    
-} while (edad === null);
+    let lastName;
+    do {
+        lastName = prompt("Ingresá tu apellido").trim();
+        if (!lastName) {
+            alert("Apellido invalido. Por favor, ingresá un apellido valido");
+        } 
+    } while (!lastName);
+    let birth;
+    let age;
+    do {
+        birth = prompt ("Ingresá tu fecha de nacimiento (dd/mm/aaaa)");
+        age = ageCalculate(birth);
+        if (age === null) {
+            alert("Por favor, ingresá una fecha válida en el formato dd/mm/aaaa");
+        }    
+    } while (age === null);
+    
 
-if (edad > 18) {
-    console.log("Excelente, eres mayor de edad, puedes comprar bebidas alcoholicas")
-} else{
-    console.log("Uhh! no eres mayor de edad, no podes comprar bebidas alcoholicas")
+    let newUser = {
+        email: email,
+        name: name,
+        lastName: lastName,
+        birth: birth,
+        age: age
+    };
+
+    users.push(newUser);
+    console.log("Usuario agregado con exito:", newUser);
+    return newUser
+    
 }
+
+
+let drinks = [
+    { 
+        img: "../img/havanaespecial",
+        drinkType: "Ron",
+        drinkName: "Havana Club",
+        price: 10000,
+        isAlcoholic: true
+    },
+    {
+        img: "../img/havana7anios",
+        drinkType: "Ron",
+        drinkName: "Havana 7 años",
+        price: 13000,
+        isAlcoholic: true
+    },
+    {
+        img: "../img/havana15anios",
+        drinkType: "Ron",
+        drinkName: "Havana 15 años",
+        price: 18000,
+        isAlcoholic: true
+    },
+    {
+        img: "../img/havanaseleccion",
+        drinkType: "Ron",
+        drinkName: "Havana Seleccion de Maestros",
+        price: 25000,
+        isAlcoholic: true
+    },
+    {
+        img: "agua",
+        drinkType: "Agua",
+        drinkName: "Agua sin gas",
+        price: 1000,
+        isAlcoholic: false
+    },
+    {
+        img: "gaseosa",
+        drinkType: "Gaseosas",
+        drinkName: "Coca Cola",
+        price: 3500,
+        isAlcoholic: false
+    },
+    {
+        img: "jugo",
+        drinkType: "Jugos",
+        drinkName: "Jugo de Naranja",
+        price: 2500,
+        isAlcoholic: false
+    },
+]
+
+function showMenu(filter = null) {
+    let menu = "Menú de bebidas: \n";
+    let filteredDrinks = drinks;
+    if (filter !== null) {
+        filteredDrinks = drinks.filter(drink => drink.isAlcoholic === filter);
+    }
+    filteredDrinks.forEach((drink, index) => {
+        menu += `${index + 1}. ${drink.drinkName} (${drink.drinkType}) - Precio: $${drink.price}\n`;
+    });
+    menu += "0. Finalizar compra\n";
+    return { menu, filteredDrinks};
+}
+
+function startPurchase(user) {
+    let total = 0;
+    let selectedItems = [];
+    let hasAlcoholicDrinks = false;
+
+    let filterChoice = prompt("¿Que tipo de bebidas deseas ver?\n1. Bebidas alcohólicas\n2. Bebidas no alcohólicas\n3. Todas las bebidas");
+    let filter = null;
+    
+    if (filterChoice == "1") {
+        if (user.age < 18) {
+            alert("Lo siento, debes tener al menos 18 años para comprar bebidas alcohólicas");
+            return;
+        }
+        filter = true;
+        } else if (filterChoice == "2") {
+            filter = false;
+        }
+
+        while (true) {
+            let { menu, filteredDrinks } = showMenu(filter);
+            let choice = parseInt(prompt(menu));
+
+            if (choice === 0) {
+                break;
+            } else if (choice > 0 && choice <= filteredDrinks.length) {
+                let selectedDrink = filteredDrinks [choice - 1];
+                selectedItems.push(selectedDrink);
+                total += selectedDrink.price;
+
+                if (selectedDrink.isAlcoholic) {
+                    hasAlcoholicDrinks = true;
+                }
+                alert(`Has añadido: ${selectedDrink.drinkName}. Total actual: $${total}`);
+            } else {
+                alert("No has seleccionado ningún producto.");
+            }
+        }
+        if (selectedItems.length > 0) {
+            let purchaseSummary = "Tu compra es:\n";
+            selectedItems.forEach(item => {
+                purchaseSummary += `- ${item.drinkName} : $${item.price}\n`;
+            });
+            purchaseSummary += `Total: $${total}`;
+            alert(purchaseSummary);
+        } else {
+            alert("No has seleccionado ningún producto");
+        }
+}
+
+let user = addUser();
+startPurchase(user);
